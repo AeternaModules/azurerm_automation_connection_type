8b778e716a97ad29d35f72f69704b3fec9c00383
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "automation_connection_types" {
-  description = "All automation_connection_type resources"
-  value       = azurerm_automation_connection_type.automation_connection_types
-}
 output "automation_connection_types_automation_account_name" {
-  description = "List of automation_account_name values across all automation_connection_types"
-  value       = [for k, v in azurerm_automation_connection_type.automation_connection_types : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_connection_types, keyed the same as var.automation_connection_types"
+  value       = { for k, v in azurerm_automation_connection_type.automation_connection_types : k => v.automation_account_name }
 }
 output "automation_connection_types_field" {
-  description = "List of field values across all automation_connection_types"
-  value       = [for k, v in azurerm_automation_connection_type.automation_connection_types : v.field]
+  description = "Map of field values across all automation_connection_types, keyed the same as var.automation_connection_types"
+  value       = { for k, v in azurerm_automation_connection_type.automation_connection_types : k => v.field }
 }
 output "automation_connection_types_is_global" {
-  description = "List of is_global values across all automation_connection_types"
-  value       = [for k, v in azurerm_automation_connection_type.automation_connection_types : v.is_global]
+  description = "Map of is_global values across all automation_connection_types, keyed the same as var.automation_connection_types"
+  value       = { for k, v in azurerm_automation_connection_type.automation_connection_types : k => v.is_global }
 }
 output "automation_connection_types_name" {
-  description = "List of name values across all automation_connection_types"
-  value       = [for k, v in azurerm_automation_connection_type.automation_connection_types : v.name]
+  description = "Map of name values across all automation_connection_types, keyed the same as var.automation_connection_types"
+  value       = { for k, v in azurerm_automation_connection_type.automation_connection_types : k => v.name }
 }
 output "automation_connection_types_resource_group_name" {
-  description = "List of resource_group_name values across all automation_connection_types"
-  value       = [for k, v in azurerm_automation_connection_type.automation_connection_types : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_connection_types, keyed the same as var.automation_connection_types"
+  value       = { for k, v in azurerm_automation_connection_type.automation_connection_types : k => v.resource_group_name }
 }
 
